c2416018a878f455b63ec6a4692c9bf9d95aae59
dcorry586 <dylancorr15@gmail.com>
2023-11-07 14:41:23 +0000

    Set front-end repo and created and tested endpoint to test database connection to backend

---
+++ app.ts
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+import { Request, Response } from "express"
+
+const express = require('express') 
+const app = express();
+const path = require('path');
+
+const appViews = path.join(__dirname, '/views');
+
+app.set('view engine', 'html');
+
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+app.listen(3000, () => {
+console.log("Server started on port 3000");
+});
+
+require('./controllers/TestConnectionController')(app);
