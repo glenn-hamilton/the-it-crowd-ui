33fcdb707322e6daea8886e3860abee25984ecc2
dcorry586 <dylancorr15@gmail.com>
2023-11-08 09:58:15 +0000

    added service class file (name changed)

---
+++ services/ConnectionService.ts
@@ -0,0 +1,14 @@
+import axiosInstance from "../config";
+
+export default class ConnectionService {
+    async testConnection(): Promise<string> {
+        try {
+            const response = await axiosInstance.get('/api/database-active');
+            const message: string = response.data;
+            console.log("message-> ", message);
+            return message;
+        } catch (e) {
+            throw new Error("Failed to test connection, " + e)
+        }
+    }
+}
