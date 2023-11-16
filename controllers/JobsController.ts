8b814b95767ea18c1d7c522e3585bbd33d429bad
dcorry586 <dylancorr15@gmail.com>
2023-11-16 15:00:46 +0000

    job by id page working

--- controllers/JobsController.ts
+++ controllers/JobsController.ts
@@ -4,18 +4,31 @@ import JobService from '../services/JobService';
 
 let jobService = new JobService();
 
-module.exports = function(app: Application) {
+module.exports = function (app: Application) {
 
-    app.get('/jobs', async (req:Request,res:Response)=>{
+    app.get('/jobs', async (req: Request, res: Response) => {
         let jobs: Job[];
-        const title:string= 'Job Roles';
+        const title: string = 'Job Roles';
 
         try {
             jobs = await jobService.getJobs();
         } catch (e) {
             console.error(e);
         }
-        res.render('JobTitles',{jobs, title});
+        res.render('JobTitles', { jobs, title });
     });
 
+
+    app.get('/jobs/:id', async (req: Request, res: Response) => {
+        const id = req.params.id; 
+        let job: Job;
+        const title: string = 'Job';
+
+        try {
+            job = await jobService.getJobById(Number(id));
+        } catch (e) {
+            console.error(e);
+        }
+        res.render('JobById', { job, title });
+    });
 };
--- services/JobService.ts
+++ services/JobService.ts
@@ -5,9 +5,19 @@ export default class JobService {
     async getJobs(): Promise<Job[]> {
         try{
             const response = await axiosInstance.get('/api/jobs');
+            // console.log("jobs --> ", response.data); 
             return response.data;
         } catch(e){
             throw new Error('Could not get jobs');
+        }
+    }
+
+async getJobById(id: number): Promise<Job> {
+        try{
+            const response = await axiosInstance.get('/api/jobs/' + id);
+            return response.data;
+        } catch(e){
+            throw new Error('Could not get job');
         }
     }
 }
